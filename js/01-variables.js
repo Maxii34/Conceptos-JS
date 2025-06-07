819c5ffd08524d954fc53da89398f84246319ff0
Maxi Ordoñez <exemaxi@outlook.com>
2025-06-07 14:58:24 -0300

    agrege ejemplos de variables constante y variables creadas con let

---
+++ js/01-variables.js
@@ -0,0 +1,23 @@
+//comentario de una sola linea
+/*
+Comentario de
+multiples
+lineas
+*/
+
+//crear una variable (let -const -var)
+
+let numero1;
+let anio = 2025 
+const comision = "c13"
+
+//Mostrar el valor de las variables
+console.log("Hola mundo")
+console.log(numero1, anio)
+
+//modificar el valor de una variable
+anio = 2025;
+
+document.writeln('<h1>Practica JS comision: '+ comision +'</h1>')
+document.writeln('<p>Contenido de la variable año: '+ anio +'</p>')
+
